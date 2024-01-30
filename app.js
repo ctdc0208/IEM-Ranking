752648c9a3f61f0fb5525d7c021ae60bc8933c89
Christian dela Cruz <66873179+ctdc0208@users.noreply.github.com>
2024-01-30 21:32:31 +0800

    Edit home

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const homeRouter = require("./routes/home")
+const homeRouter = require('./routes/home');
 
 var app = express();
 
